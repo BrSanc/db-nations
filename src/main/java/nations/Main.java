c296986abf97d95dc4d3814f700521baaf1a9421
Sanchez <bradsanchez9845@gmail.com>
2023-09-07 13:10:25 +0200

    MILESTONE 4(Bonus)

--- src/main/java/nations/Main.java
+++ src/main/java/nations/Main.java
@@ -1,6 +1,8 @@
 package nations;
 
+import java.math.BigDecimal;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -27,6 +29,18 @@ public class Main {
             "join countries c on c.region_id = r.region_id\n" +
             "where c.name like ? \n" +
             "order by c.name;";
+    private final static String SQL_NATION_LANGUAGUE_STATS = "select \n" +
+            "c.country_id as \"IdNation\",\n" +
+            "c.name as  \"NameNation\",\n" +
+            "l.`language` as \"LanguageNation\",\n" +
+            "CS.`year` as \"Year\",\n" +
+            "cs.population as \"PopulationNation\",\n" +
+            "cs.gdp as \"GDP_Nation\"\n" +
+            "from countries c \n" +
+            "join country_languages cl on c.country_id = cl.country_id \n" +
+            "join languages l on cl.language_id = l.language_id \n" +
+            "join country_stats cs on c.country_id = cs.country_id \n" +
+            "where c.country_id  = ? and cs.`year` = 2018";
     private final static String SQL_TEST = "select * from countries c where name like ?;";
     public static void main(String[] args) {
 
@@ -66,6 +80,49 @@ public class Main {
                     }
                 }
             }
+
+            System.out.print("Choose a country id: ");
+            int idChoised = Integer.parseInt(scan.nextLine());
+
+            try(PreparedStatement ps = conn.prepareStatement(SQL_NATION_LANGUAGUE_STATS)){
+                ps.setInt( 1,idChoised);
+                try(ResultSet rs = ps.executeQuery()) {
+                    String nationName = null;
+                    int nationId = 0;
+                    String languages = null;
+                    int year = 0;
+                    int population = 0;
+                    BigDecimal gdp = null;
+                    ArrayList<String> languagesList = new ArrayList<>();
+
+                    while (rs.next()) {
+                        nationId = rs.getInt("IdNation");
+                        nationName = rs.getString("NameNation");
+                        languages = rs.getString("LanguageNation");
+                        year = rs.getInt("Year");
+                        population = rs.getInt("PopulationNation");
+                        gdp = rs.getBigDecimal("GDP_Nation");
+
+                        languagesList.add(languages);
+                    }
+                    System.out.println();
+                    System.out.println("Details for country: " + nationName);
+
+                    System.out.print("Languages: ");
+                    for (int i = 0; i < languagesList.size(); i++) {
+                        System.out.print(languagesList.get(i) + ", ");
+                    }
+                    System.out.println();
+                    System.out.println("Most recent stats");
+                    System.out.println("Year: " + year);
+                    System.out.println("Population: "+ population);
+                    System.out.println("GPD: "+ gdp);
+
+                    //System.out.println(nationName + " | " + nationId + " | " + languages + " | " + year + " | " + population + " | " + gdp);
+
+                }
+            }
+
 
 
 
