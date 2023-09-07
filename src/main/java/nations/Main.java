5ecefa8d545de912c32aeda5b15a02438fc8f6f2
Sanchez <bradsanchez9845@gmail.com>
2023-09-07 10:36:41 +0200

    MILESTONE 2 - connection

--- src/main/java/nations/Main.java
+++ src/main/java/nations/Main.java
@@ -1,9 +1,6 @@
 package nations;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
@@ -11,26 +8,36 @@ public class Main {
     private final static String DB_URl ="jdbc:mysql://localhost:3306/dump_nations";
     private final static String DB_USER = "root";
     private final static String DB_PASSWORD = "root";
-    private final static String SQL_TABLE = "select \n" +
-            "c.name as \"Nome Nazioni\", \n" +
-            "c.country_id as \"Id Nazioni\",\n" +
-            "r.name as \"Nome Regione\",\n" +
-            "c2.name as \"Nome Continente\"\n" +
+    private final static String SQL_NATIONS = "select \n" +
+            "c.name as \"NomeNazioni\", \n" +
+            "c.country_id as \"IdNazioni\",\n" +
+            "r.name as \"NomeRegione\",\n" +
+            "c2.name as \"NomeContinente\"\n" +
             "from continents c2 \n" +
             "join regions r on r.continent_id = c2.continent_id\n" +
             "join countries c on c.region_id = r.region_id\n" +
             "order by c.name;";
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+
 
         try(Connection conn = DriverManager.getConnection(DB_URl,DB_USER,DB_PASSWORD)){
 
-            try(PreparedStatement ps = conn.prepareStatement(SQL_TABLE)){
+            try(PreparedStatement ps = conn.prepareStatement(SQL_NATIONS)){
                 try(ResultSet rs = ps.executeQuery()){
+                    while (rs.next()){
+                        String nationName = rs.getString("NomeNazioni");
+                        int nationId = rs.getInt("IdNazioni");
+                        String regionsName = rs.getString("NomeRegione");
+                        String continentsName = rs.getString("NomeContinente");
 
+                        System.out.println(nationName + " | " + nationId + " | " + regionsName + " | " + continentsName) ;
+                    }
                 }
             }
 
+        }catch(SQLException exception){
+            System.out.println("An error occurred");
         }
 
 
